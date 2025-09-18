a46b5987e23506b0435004af39d1c11c0d46d0fc
Morris Israel Nyakeh Abu <morrisisraelnabou@gmail.com>
2025-09-18 13:37:09 +0000

    Using components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,15 @@
+import Card from "@/components/Card";
 
 const Landing = () => {
     return (
         <div>
             <h1 className="text-4xl font-extralight">Landing Page</h1>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
         </div>)
 }
 
